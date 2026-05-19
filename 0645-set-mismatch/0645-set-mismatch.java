b4413516f0e9edfae4494c7f0041f88dbc2d1b6d
Muhammad Saad <126744195+Saadcui@users.noreply.github.com>
2026-05-19 22:59:02 +0500

    Time: 2 ms (95.41%), Space: 47.9 MB (18.26%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        int[] freq = new int[n + 1];
+
+        int duplicate = -1;
+        int missing = -1;
+
+        // Count frequency of each number
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        // Find duplicate and missing numbers
+        for (int i = 1; i <= n; i++) {
+            if (freq[i] == 2) {
+                duplicate = i;
+            } else if (freq[i] == 0) {
+                missing = i;
+            }
+        }
+
+        return new int[]{duplicate, missing};
+    }
+}
